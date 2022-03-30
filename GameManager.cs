13ee2f6c992a9d0d17e46ad2cdc014cfc596391f
navysys <51705795+navysys@users.noreply.github.com>
2022-03-30 22:03:00 +0900

    base

---
+++ GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Relic[] RelicList;
+
+    public GameObject Player;
+    public GameObject Enemy;
+
+    enum GameState { Start, Main, Reward, End };
+    GameState State = GameState.Start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
