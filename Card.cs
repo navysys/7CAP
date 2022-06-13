2f61434c06e4de624b269e404aa503f5405ad9a3
bukyong <pbg736837@gmail.com>
2022-06-14 04:08:17 +0900

    test

---
+++ Card.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int c_num;
+    public string c_name;
+    public Sprite c_image;
+    public int c_cost;
+    public string c_text;
+
+    public void C_Effect()
+	{
+
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    GameManager GM;
+    
+    public int HP;
+    public int Def;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
